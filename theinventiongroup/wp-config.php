9a40b5c47fd5520d6a6a3d5ec736a77774a7b4ea
Stephany Reynolds <sreynolds9@my.wctc.edu>
2020-03-11 15:06:07 -0500

    hide php errors in wp

--- theinventiongroup/wp-config.php
+++ theinventiongroup/wp-config.php
@@ -77,7 +77,10 @@ $table_prefix = 'invgrp2020_';
  *
  * @link https://codex.wordpress.org/Debugging_in_WordPress
  */
-define( 'WP_DEBUG', false );
+ini_set('display_errors','Off');
+ini_set('error_reporting', E_ALL );
+define('WP_DEBUG', false);
+define('WP_DEBUG_DISPLAY', false);
 
 /* That's all, stop editing! Happy publishing. */
 
